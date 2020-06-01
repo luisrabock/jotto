eeb3a0fecc12bf5d1e895facadbbae661f163fc6
Luis <luis@rocketlab.com.br>
2020-06-01 20:22:31 -0300

    GuessedWords unit tests finish

--- src/__tests__/GuessedWords.test.js
+++ src/__tests__/GuessedWords.test.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { findByTestAttr, checkProps } from '../../tests/testUtils';
-import GuessedWords from '../GuessedWords';
+import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAttr, checkProps } from "../../tests/testUtils";
+import GuessedWords from "../GuessedWords";
 
-const defaultProps = {guessedWords: [{guessedWord: 'train', letterMatchCount: 3}]};
+const defaultProps = {
+  guessedWords: [{ guessedWord: "train", letterMatchCount: 3 }],
+};
 
-const setup = (props={}) => {
-    const setupProps = {...defaultProps, ...props};
-    return shallow(<GuessedWords {...setupProps} />)
-}
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<GuessedWords {...setupProps} />);
+};
 
-it('Does not throw a Warning with expected props', () => {
-    checkProps(GuessedWords, defaultProps);
-})
+it("Does not throw a Warning with expected props", () => {
+  checkProps(GuessedWords, defaultProps);
+});
+
+describe("if there are no words guessed", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: [] });
+  });
+  it("renders without error", () => {
+    const component = findByTestAttr(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
+  it("renders instructions to guess a word", () => {
+    const instructions = findByTestAttr(wrapper, "guess-instructions");
+    expect(instructions.text().length).not.toBe(0);
+  });
+});
+
+describe("if there are words guessed", () => {});
--- src/__tests__/congrats.test.js
+++ src/__tests__/congrats.test.js
@@ -1,36 +1,35 @@
-import React from 'react';
-import {shallow} from 'enzyme';
-import checkPropTypes from 'check-prop-types';
+import React from "react";
+import { shallow } from "enzyme";
+import checkPropTypes from "check-prop-types";
 
-import Congrats from '../Congrats';
-import { findByTestAttr, checkProps } from '../../tests/testUtils';
+import Congrats from "../Congrats";
+import { findByTestAttr, checkProps } from "../../tests/testUtils";
 
-const defaultProps = {success: true};
-const setup = (props={}) => {
-    const setupProps = {...defaultProps, ...props};
-    return shallow(<Congrats {...setupProps}/>)
-}
+const defaultProps = { success: true };
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Congrats {...setupProps} />);
+};
 
-it('Renders without error', () => {
-    const wrapper = setup({success: false});
-    const component = findByTestAttr(wrapper, 'component-congrats');
-    expect(component.length).toBe(1);
-})
+it("Renders without error", () => {
+  const wrapper = setup({ success: false });
+  const component = findByTestAttr(wrapper, "component-congrats");
+  expect(component.length).toBe(1);
+});
 
-it('Renders no text when `Success` prop is false', () => {
-    const wrapper = setup({success: false});
-    console.log('wrapper..', wrapper);
-    const component = findByTestAttr(wrapper, 'component-congrats');
-    expect(component.text()).toBe('');
-})
+it("Renders no text when `Success` prop is false", () => {
+  const wrapper = setup({ success: false });
+  const component = findByTestAttr(wrapper, "component-congrats");
+  expect(component.text()).toBe("");
+});
 
-it('Renders no empty congrats when `success` prop is true', () => {
-    const wrapper = setup({success: true});
-    const message = findByTestAttr(wrapper, 'congrats-message');
-    expect(message.text().length).not.toBe(0);
-})
+it("Renders no empty congrats when `success` prop is true", () => {
+  const wrapper = setup({ success: true });
+  const message = findByTestAttr(wrapper, "congrats-message");
+  expect(message.text().length).not.toBe(0);
+});
 
-it('Does not throw a warning with expected props', () => {
-    const expectedProps = {success: false};
-    checkProps(Congrats, expectedProps);
-})
+it("Does not throw a warning with expected props", () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps);
+});
