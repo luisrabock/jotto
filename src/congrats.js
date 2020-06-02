e0cf9d579a7a39f804762b78267ff724b71acad5
Luis <luis@rocketlab.com.br>
2020-06-01 21:02:07 -0300

    Add bootstrap and styling the jotto app

--- src/congrats.js
+++ src/congrats.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 const Congrats = (props) => {
-    if(props.success) {
-        return (
-            <div data-test="component-congrats">
-                <span data-test="congrats-message">
-                    Congratulations! You guessed the world!
-                </span>
-            </div>
-        )
-    } else {
-        return <div data-test="component-congrats"></div>
-    }
-}
+  if (props.success) {
+    return (
+      <div data-test="component-congrats" className="alert alert-success">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the world!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats"></div>;
+  }
+};
 
 Congrats.propTypes = {
-    success: PropTypes.bool.isRequired,
-}
+  success: PropTypes.bool.isRequired,
+};
 
 export default Congrats;
